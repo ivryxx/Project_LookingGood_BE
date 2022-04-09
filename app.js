4c39cc4ce51e059b5418930fad21dfd47bacd872
Hoya11 <88309582+Hoya11@users.noreply.github.com>
2022-04-09 14:58:29 +0900

    app ing

--- app.js
+++ app.js
@@ -6,5 +6,17 @@ const port = 3000;
 
 connect();
 
+const postsRouter = require("./routes/posts");
+const usersRouter = require("./routes/users");
+const commentsRouter = require("./routes/comments");
+
 app.use(cors());
+app.use(express.static("static"));
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use("/api", [postsRouter, usersRouter, commentsRouter]);
+
+app.listen(port, () => {
+  console.log(port, "포트가 켜졌습니다.");
+});
