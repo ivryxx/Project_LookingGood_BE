d58458796fb8551a6428fd5c5029919973ba8047
IcandoDeveloper <96129738+IcandoDeveloper@users.noreply.github.com>
2022-04-14 11:32:10 +0900

    ddd

--- routes/posts.js
+++ routes/posts.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const aws = require("aws-sdk");
+const multer = require("multer");
+const multerS3 = require("multer-s3");
 const router = express.Router();
 const authmiddlewares = require("../middlewares/auth-middleware");
 const Post = require("../schemas/post");
@@ -7,16 +10,9 @@ const Comment = require("../schemas/comment")
 const upload = require('../modules/multer');
 const req = require("express/lib/request");
 const res = require("express/lib/response");
-
-
 router.use(bodyParser.urlencoded({ extended: false }));
 router.use(bodyParser.json());
-
-
-
-
 // 이미지 파일 AWS S3 저장
-
 // router.post(
 //   "/single",
 //   upload.single("imageUrl"), async (req, res) => {
@@ -28,11 +24,8 @@ router.use(bodyParser.json());
 //     } catch (e) {
 //       console.log(e)
 //     }
-
 //   });
-
 // UserController.uploadImage,
-
 router.post('/single', upload.single('imageUrl'), async (req, res) => {
   const file = await req.file;
   console.log(file);
@@ -41,12 +34,9 @@ router.post('/single', upload.single('imageUrl'), async (req, res) => {
     console.log(result)
     res.status(200).json({ imageUrl: result })
   } catch (e) {
-
   }
 });
-
 // 게시글 작성 //
-
 router.post("/post", authmiddlewares, upload.single('imageUrl'), async (req, res) => {
   const today = new Date();
   const year = today.getFullYear();
@@ -55,109 +45,102 @@ router.post("/post", authmiddlewares, upload.single('imageUrl'), async (req, res
   let hour = today.getHours();
   let minutes = today.getMinutes();
   let seconds = today.getSeconds();
-
   month = month < 10 ? "0" + month : month;
   day = day < 10 ? "0" + day : day;
   hour = hour < 10 ? "0" + hour : hour;
   seconds = seconds < 10 ? "0" + seconds : seconds;
   minutes = minutes < 10 ? "0" + minutes : minutes;
-
-  const createAt =
+  const date =
     year + "-" + month + "-" + day + " " + hour + ":" + minutes + ":" + seconds;
-
-  const { user } = res.locals;
-  const userId = user[0].userId;
-  const userImageUrl = user[0].userImageUrl;
   const imageUrl = req.file.location
   const { category, title, content } = req.body;
-  console.log(userId)
-
+  // let { user } = res.locals;
   await Post.create({
-    // postId: postId,
-    userId,
-    userImageUrl,
     category: category,
     title: title,
     imageUrl,
     content: content,
-    createAt: createAt,
+    date: date,
   });
-  res.json({ userId, userImageUrl, category, title, imageUrl, content });
+  res.json({ category, title, imageUrl, content });
 });
-
 // 게시글 삭제
-
 // router.delete("/post/delete/:postId", authmiddlewares, async (req, res) => {
 //   await Post.deleteOne({ _id: req.params.postId })
 //   console.log(req.params)
 //   res.json({ success: "삭제 성공" });
 // });
-
-
 router.delete('/post/delete/:postId', authmiddlewares, async (req, res) => { //게시글 삭제
   const { postId } = req.params;
   console.log(req.params)
   await Post.deleteOne({ _id: postId });
   res.json({ success: "삭제가 완료 되었습니다" });
 });
-
 // 게시글 수정
-
 router.put("/post/put/:postId", upload.single('imageUrl'), authmiddlewares, async (req, res) => {
-  const { postId } = req.params
+  const today = new Date();
+  const year = today.getFullYear();
+  let month = today.getMonth() + 1;
+  let day = today.getDate();
+  let hour = today.getHours();
+  let minutes = today.getMinutes();
+  let seconds = today.getSeconds();
+  month = month < 10 ? "0" + month : month;
+  day = day < 10 ? "0" + day : day;
+  hour = hour < 10 ? "0" + hour : hour;
+  seconds = seconds < 10 ? "0" + seconds : seconds;
+  minutes = minutes < 10 ? "0" + minutes : minutes;
+  const date =
+    year + "-" + month + "-" + day + " " + hour + ":" + minutes + ":" + seconds;
+  const { postId } = req.params;
   const { category, title, content } = req.body;
   const imageUrl = req.file.location;
-  const post = Post.findOne({ _id: postId });
-  if (!post) {
-    return res.status(400).send({ errorMessage: "본인의 글이 아닙니다." })
-  }
-  await Post.updateOne({ _id: postId }, {
-    $set: {
-      // postId: postId,
-      // userId: userId,
-      category,
-      title,
-      imageUrl,
-      content,
-      // date: date
-    },
-  })
-  res.json({ success: true, message: "수정이 완료됐습니다." });
+  await Post.updateOne({ _id: postId }),
+    {
+      $set: {
+        category,
+        title,
+        imageUrl,
+        content,
+        date
+      },
+    };
+  res.json({ success: "수정이 완료됐습니다!!!!!!" });
 });
-
-// const { postId } = req.params;
+// //
+// router.get("/post/find/:postId", authmiddlewares, async (req, res) => {
+//   const { postId } = req.params;
+//   console.log(req.params);
+//   // await Post.findOneById({ _id: postId });
+//   const { user } = res.locals;
+//   const userId = user[0].userId;
+//   const userImageUrl = user[0].userImageUrl;
+//   const imageUrl = req.file.location
 //   const { category, title, content } = req.body;
-//   const imageUrl = req.file.location;
-//   await Post.findByIdAndUpdate({ _id: postId }),
-//     {
-//       $set: {
-//         category,
-//         title,
-//         imageUrl,
-//         content,
-//         date
-//       },
-//     };
-//   res.json({ success: "수정이 완료됐습니다!!!!!!" });
+//   console.log(userId, userImageUrl, imageUrl, category, title, content)
+//   await Post.findById({ _id: postId });({
+//     // postId: postId,
+//     userId,
+//     userImageUrl,
+//     category: category,
+//     title: title,
+//     imageUrl,
+//     content: content,
+//     createAt: createAt,
+//   });
+//   res.json({ userId, userImageUrl, category, title, imageUrl, content });
 // });
-
-
-
 // 전체 게시글 조회 //
-
 router.get("/post", async (req, res) => {
   const Posts = await Post.find();
   res.json({ list: Posts });
 });
-
 // 상세 페이지 접속
-
-
-router.get('/post/:postId', async function (req, res) {
+router.get('/post/detail/:postId', async function (req, res) {
   const { postId } = req.params;
   Post.findById(postId, async function (err, post) {
     if (!err) {
-      let comments = await Comment.find({ postId: postId });
+      let comments = await Comment.find({ _id : postId });
       comments.sort(function (a, b) {
         return b.updatedAt - a.updatedAt;
       });
@@ -167,15 +150,22 @@ router.get('/post/:postId', async function (req, res) {
     }
   });
 });
-
-
-
-
-
-
-
+//새로고침 후 다시 조회
+// router.get("/post/reload/:postId", async function (req, res) {
+//   const { postId } = req.params;
+//   Post.findById(postId, async function (err, post) {
+//     if (!err) {
+//       let comments = await Comment.find({ postId: postId });
+//       comments.sort(function (a, b) {
+//         return b.updatedAt - a.updatedAt;
+//       });
+//       res.json({ ok: true, post, comments });
+//     } else {
+//       res.json({ ok: false, post: {}, comments: {} });
+//     }
+//   });
+// });
 router.get('/post/')
-
 // router.get("/post/:postId", async (req, res) => {
 //   console.log(req.params)
 //   const Posts = await Post.findById(req.params.postId);
@@ -184,5 +174,18 @@ router.get('/post/')
 //   res.json({ list: Posts, comment });
 //   console.log(s)
 // });
-
 module.exports = router;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
